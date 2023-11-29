fb5ebd2e8b395e690026db0df43d3baebb4640aa
gabriel-ferreira-da-silva <gab_fs@outlook.com>
2023-11-29 09:57:03 -0300

    nova versão de sparse-table, update e query

--- sparse-table/sparse-table.h
+++ sparse-table/sparse-table.h
@@ -3,7 +3,69 @@
 #include<cmath>
 // creates the pre processed table
 
-std::vector<std::vector<int>> createTable( int (*func) (std::vector<int> , int , int ) , int (*mu) (int,int),int neutro, std::vector<int> A ){
+
+#define WIDTH  10000
+#define HEIGHT 1000
+
+#define A_ 1664525
+#define C_ 1013904223
+#define R_ 4294967296
+
+
+#define MIN 0
+#define MAX 4294967295
+
+long long int table[HEIGHT][WIDTH];
+long long int A[WIDTH];
+
+
+void createTable( long long int (*func) (long long int, long long int) , long long int N ){
+    int m = (long long int) log2(N) + 1;
+
+    for(long long int i =0; i < N ; i++){
+        table[0][i] = A[i];
+    }
+
+    int k = 1;
+    for(int i =1 ; i < m ; i++){
+        for(int j=0; j < N ; j++){
+            if(j+k < N){
+                table[i][j] = func(table[i-1][j], table[i-1][j+k]);
+                //table[i][j] = table[i-1][j] + table[i-1][j+k];
+            }else{
+                table[i][j] = table[i-1][j];
+            }
+        }
+        k*=2;
+    }
+}
+
+long long int query(long long int (*func) (long long int, long long int) ,long long int N, long long int left, long long int right, long long int neutro){
+    long long int ans = neutro;
+    int i;
+    while(left < right){
+        i = (long long int) log2(right-left);
+        ans = func(ans , table[i][left]);
+        left= left + pow(2, i);
+    }
+
+    return ans;
+}
+
+void update(long long int (*func) (long long int, long long int) ,long long int N, long long int j, long long int val, long long int neutro){
+    table[0][j]= val;
+    long long int k=1;
+    long long int m = (long long int) log2(N) +1;
+    for(long long int i=1; i<m ; i++){
+        for(long long int left =( (0 > j-2*k +1) ? 0 :j-2*k +1 ); left <= j ; left++){
+            table[i][left] = func(table[i-1][left], table[i-1][left+k]);
+            //table[i][left] = table[i-1][left] + table[i-1][left+k];
+        }
+        k*=2;
+    }
+}
+
+std::vector<std::vector<int>> createTable_( int (*func) (std::vector<int> , int , int ) , int (*mu) (int,int),int neutro, std::vector<int> A ){
     int m = (int) log2(A.size()) + 1;
     int n = A.size();
     std::vector<std::vector<int>> table;
